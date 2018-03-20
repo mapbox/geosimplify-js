daefdf733cc49f0a492de0c3f72f2c745d41101a
Daniel Patterson <daniel@mapbox.com>
2018-03-19 22:38:32 -0700

    Clean up package.json for release.

--- geosimplify.js
+++ geosimplify.js
@@ -1,17 +1,17 @@
 /*
- (c) 2017, Vladimir Agafonkin
+ (c) 2017, Mapbox
+
+ Based on simplify-js (c) 2017, Vladimir Agafonkin
  Simplify.js, a high-performance JS polyline simplification library
  mourner.github.io/simplify-js
 */
-
-(function () { 'use strict';
-
+'use strict';
 var cheapRuler = require('cheap-ruler');
 
 var rulerCache = {};
 
 function getRuler(latitude) {
-    // Cache rulers every 0.00001 degrees of latitude
+// Cache rulers every 0.00001 degrees of latitude
     var roundedLatitude = Math.round(latitude * 100000);
     if (rulerCache[roundedLatitude] === undefined) {
         rulerCache[roundedLatitude] = cheapRuler(latitude, 'meters');
@@ -27,7 +27,7 @@ function getDist(p1, p2) {
 // Distance from a point to a segment (line between two points) in metres
 function getSegDist(p, p1, p2) {
     var ruler = getRuler(p[1]);
-    var pointOnLine = ruler.pointOnLine([p1,p2], p).point;
+    var pointOnLine = ruler.pointOnLine([p1, p2], p).point;
     return ruler.distance(p, pointOnLine);
 }
 
@@ -44,8 +44,8 @@ function simplifyDPStep(points, first, last, offsetTolerance, gapTolerance, simp
         }
     }
 
-    // Don't remove a point if it would create a segment longer
-    // than gapTolerance
+// Don't remove a point if it would create a segment longer
+// than gapTolerance
     var firstLastDist = getDist(points[first], points[last]);
 
     if (maxDistanceFound > offsetTolerance || firstLastDist > gapTolerance) {
@@ -66,16 +66,8 @@ function simplifyDouglasPeucker(points, offsetTolerance, gapTolerance) {
 
 function simplify(points, offsetTolerance, gapTolerance) {
     if (points.length <= 2) return points;
-    points = simplifyDouglasPeucker(points, offsetTolerance, gapTolerance );
+    points = simplifyDouglasPeucker(points, offsetTolerance, gapTolerance);
     return points;
 }
 
-// export as AMD module / Node module / browser or worker variable
-if (typeof define === 'function' && define.amd) define(function() { return simplify; });
-else if (typeof module !== 'undefined') {
-    module.exports = simplify;
-    module.exports.default = simplify;
-} else if (typeof self !== 'undefined') self.simplify = simplify;
-else window.simplify = simplify;
-
-})();
+module.exports = simplify;
--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+'use strict';
 var geosimplify = require('../geosimplify'),
     tape = require('tape'),
     fs = require('fs');
@@ -5,7 +6,7 @@ var geosimplify = require('../geosimplify'),
 var singapore = JSON.parse(fs.readFileSync('./test/fixtures/singapore.geojson'));
 var svalbard = JSON.parse(fs.readFileSync('./test/fixtures/svalbard.geojson'));
 
-tape('Basic fixture tests - larger threshold = more points removed', function(t) {
+tape('Basic fixture tests - larger threshold = more points removed', function (t) {
     var result = geosimplify(singapore.features[0].geometry.coordinates, 50, 1000);
     t.equal(result.length, 3, 'Reduced to 9 coords');
     result = geosimplify(singapore.features[0].geometry.coordinates, 5, 1000);
@@ -28,29 +29,29 @@ tape('Basic fixture tests - larger threshold = more points removed', function(t)
     t.end();
 });
 
-tape('Latitude-sensitive removal', function(t) {
+tape('Latitude-sensitive removal', function (t) {
 
     // 100m line with a 4.45m kink in the middle
-    var equator_latitude_5m_offset = [[0,0], [0.0005,0.00004], [0.001,0] ];
-    result = geosimplify(equator_latitude_5m_offset, 4.40, 1000);
+    var equator_latitude_5m_offset = [[0, 0], [0.0005, 0.00004], [0.001, 0]];
+    var result = geosimplify(equator_latitude_5m_offset, 4.40, 1000);
     t.equal(result.length, 3, 'Removed no coordinates');
     result = geosimplify(equator_latitude_5m_offset, 5.0, 1000);
     t.equal(result.length, 2, 'Removed one coordinates');
 
-    var arctic_latitude_5m_offset = [[0,67], [0.0005,67.00004], [0.001,67] ];
+    var arctic_latitude_5m_offset = [[0, 67], [0.0005, 67.00004], [0.001, 67]];
     result = geosimplify(arctic_latitude_5m_offset, 4.40, 1000);
     t.equal(result.length, 3, 'Removed no coordinates');
     result = geosimplify(arctic_latitude_5m_offset, 5.0, 1000);
     t.equal(result.length, 2, 'Removed one coordinates');
 
-    var equator_longitude_5m_offset = [[0,0], [0.00004,0.0005], [0,0.001]];
+    var equator_longitude_5m_offset = [[0, 0], [0.00004, 0.0005], [0, 0.001]];
     result = geosimplify(equator_longitude_5m_offset, 4.40, 1000);
     t.equal(result.length, 3, 'Removed no coordinates');
     result = geosimplify(equator_longitude_5m_offset, 5.0, 1000);
     t.equal(result.length, 2, 'Removed one coordinates');
 
     // Note the middle longitude here - this is 4.45m at 67 latitude
-    var arctic_longitude_5m_offset = [[0,67], [0.0001024,67.0005], [0,67.001]];
+    var arctic_longitude_5m_offset = [[0, 67], [0.0001024, 67.0005], [0, 67.001]];
     result = geosimplify(arctic_longitude_5m_offset, 4.40, 1000);
     t.equal(result.length, 3, 'Removed no coordinates');
     result = geosimplify(arctic_longitude_5m_offset, 5.0, 1000);
